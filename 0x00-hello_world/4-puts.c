ea41b465aac511bad053321ddaf920f949b04ce6
Vikryan <vikryan1@gmail.com>
2023-06-15 16:11:20 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - 4-puts.c
+ * description - Write a program that prints exactly what is given
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    Puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
