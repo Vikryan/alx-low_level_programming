7f03d033a52ca2991572605d8ab714799612a7ca
Vikryan <vikryan1@gmail.com>
2023-06-21 12:19:40 -0700

    Printing alphabet in lowercase and in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Printing alphabets in lowercase and in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char p;
+
+	p = 'z';
+	while
+		(p >= 'a')
+		{
+			putchar(p);
+			p--;
+		}
+	putchar('\n');
+	return (0);
+}
