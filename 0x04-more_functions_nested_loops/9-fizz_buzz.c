7fffe8163d210fd19713339557a85c101636b9b7
Vikryan <vikryan1@gmail.com>
2023-07-03 19:19:20 -0700

    A function printing number from 1 to 100 xcept multiples of 3 and 5 printed as Fizz and Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,42 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - A function prints numbers 1 to 100
- * its instead prints Fizz for multiples of three
- * and prints Buzz for multiples of five
- * Return: 0 on success
+ * main - A function printing numbers from 1 t 100
+ * but prints Fizz and Buzz for multipls of 3 and 5
+ * Return: 0 (success)
  */
 int main(void)
 {
-	int m;
+	int k;
+	char x[] = "Fizz";
+	char p[] = "Buzz";
+	char xp[] = "FizzBuzz";
 
-	for (m = 1; m <= 100; m++)
+	for (k = 1; 1 <= 100; k++)
 	{
-		if (m % 3 == 0 && m % 5 != 0)
-		{
-			printf("Fizz");
-		}
-		else if
-			(m % 5 == 0 && m % 3 != 0)
-			{
-				printf("Buzz");
-			}
-		else if
-			(m % 3 == 0 && m % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-		else if
-			(m == 1)
-			{
-				printf("%d", m);
-			}
+		if (k == 100)
+			printf("%s", p);
+		else if ((k % 3 == 0) && (k % 5 == 0))
+			printf("%s", xp);
+		else if (k % 3 == 0)
+			printf("%s", x);
+		else if (k % 5 == 0)
+			printf("%s", p);
 		else
-		{
-			printf("%d", m);
-		}
+			printf("%d", k);
 	}
 	printf("\n");
-
-	return (0);
 }
