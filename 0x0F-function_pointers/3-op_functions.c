66de451454f5884f7234afeedba86637a1cba92d
Vikryan <vikryan1@gmail.com>
2023-07-20 19:23:39 -0700

    A function that performs simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,62 +1,62 @@
 #include "3-calc.h"
 
-int op_add(int x, int p);
-int op_sub(int x, int p);
-int op_mul(int x, int p);
-int op_div(int x, int p);
-int op_mod(int x, int p);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
  * op_add - Returns sum
- * @x: first number
- * @p: second number
+ * @a: first number
+ * @b: second number
  *
- * Return: The sum of x and p
+ * Return: The sum of a and b
  */
-int op_add(int x, int p)
+int op_add(int a, int b)
 {
-	return (x + p);
+	return (a + b);
 }
 
 /**
  * op_sub - Returns the difference
- * @x: first number
- * @p: second number
+ * @a: first number
+ * @b: second number
  */
-int op_sub(int x, int p)
+int op_sub(int a, int b)
 {
-	return (x - p);
+	return (a - b);
 }
 /**
  * op_mul - Returns the product
- * @x: first number
- * @p: second number
+ * @a: first number
+ * @b: second number
  *
- * Return: The product of x and p
+ * Return: The product of a and b
  */
-int op_mul(int x, int p)
+int op_mul(int a, int b)
 {
-	return (x * p);
+	return (a * b);
 }
 /**
  * op_div - Returns the division of numbers
- * @x: first number
- * @p: second number
+ * @a: first number
+ * @b: second number
  *
  * Return: The quotient of numbers
  */
-int op_div(int x, int p)
+int op_div(int a, int b)
 {
-	return (x / p);
+	return (a / b);
 }
 /**
  * op_mod - Returns the remainder of the division of numbers
- * @x: first number
- * @p: second number
+ * @a: first number
+ * @b: second number
  *
  * Return: Remainder of division of two numbers
  */
-int op_mod(int x, int p)
+int op_mod(int a, int b)
 {
-	return (x % p);
+	return (a % b);
 }
